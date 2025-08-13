f408d9204dbf8734d7c752d82abbe7415bb5da97
andy_wendy <alexkens93@gmail.com>
2025-08-13 14:11:11 +0200

    finish contact form, change logo

--- js/contact-form-button.js
+++ js/contact-form-button.js
@@ -1,7 +1,18 @@
+/*
 const btn = document.getElementById("form-button");
-
 btn.addEventListener("click", () => {
     const response = document.createElement("p");
     response.innerText = "Thank you for sending your data. It will be used against you!";
     document.getElementById("response").appendChild(response);
 });
+*/
+
+function formFunction() {
+    event.preventDefault();
+
+    const response = document.createElement("p");
+    response.innerText = "Thank you for sending your data. It will be used against you!";
+    response.style.fontSize = "50px";
+    response.style.textAlign = "center";
+    document.getElementById("response").appendChild(response);
+}
